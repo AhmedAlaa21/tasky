fecc565a266f75e55e2bfffd7f9ccb2370b7b4c0
AhmedAlaa21 <ahmedmohamedalaa96@gmail.com>
2026-06-28 21:52:24 +0300

    get started functionality

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Tasky',
       theme: ThemeData(useMaterial3: true),
-      home: const WelcomeScreen(),
+      home: WelcomeScreen(),
     );
   }
 }
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:todo_app/screens/home_screen.dart';
 
 class WelcomeScreen extends StatelessWidget {
-  const WelcomeScreen({super.key});
+  WelcomeScreen({super.key});
 
-  void getStarted() {}
+  final TextEditingController textEditingController = TextEditingController();
+  final GlobalKey<FormState> _key = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -14,113 +16,138 @@ class WelcomeScreen extends StatelessWidget {
       body: SafeArea(
         child: SingleChildScrollView(
           child: Center(
-            child: Column(
-              children: [
-                SizedBox(height: 16),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SvgPicture.asset("assets/images/logo.svg"),
-                    SizedBox(width: 8),
-                    Text(
-                      "Tasky",
-                      style: TextStyle(
-                        color: Color(0xFFFFFFFF),
-                        fontSize: 28,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 118),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Welcome to Tasky",
-                      style: TextStyle(
-                        color: Color(0xFFFFFCFC),
-                        fontSize: 24,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                    SvgPicture.asset("assets/images/waving-hand.svg"),
-                  ],
-                ),
-                SizedBox(height: 8),
-                Text(
-                  "Your productivity journey starts here.",
-                  style: TextStyle(
-                    color: Color(0xFFFFFCFC),
-                    fontWeight: FontWeight.w400,
-                    fontSize: 16,
-                  ),
-                ),
-                SizedBox(height: 24),
-                SvgPicture.asset(
-                  "assets/images/work-in-progress.svg",
-                  width: 215,
-                  height: 200,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
+            child: Form(
+              key: _key,
+              child: Column(
+                children: [
+                  SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      SizedBox(height: 28),
+                      SvgPicture.asset("assets/images/logo.svg"),
+                      SizedBox(width: 8),
                       Text(
-                        "Full Name",
+                        "Tasky",
                         style: TextStyle(
-                          color: Color(0xFFFFFCFC),
+                          color: Color(0xFFFFFFFF),
+                          fontSize: 28,
                           fontWeight: FontWeight.w400,
-                          fontSize: 16,
                         ),
-                      ),
-                      SizedBox(height: 8),
-                      TextField(
-                        style: TextStyle(color: Colors.white),
-                        decoration: InputDecoration(
-                          hintText: "e.g. Ahmed Alaa",
-                          hintStyle: TextStyle(color: Color(0xFF6D6D6D)),
-                          filled: true,
-                          fillColor: Color(0xFF282828),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(16),
-                            borderSide: BorderSide.none,
-                          ),
-                        ),
-                        cursorColor: Colors.white,
                       ),
                     ],
                   ),
-                ),
-                SizedBox(height: 28),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: SizedBox(
-                    height: 40,
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: getStarted,
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Color(0xFF15B86C),
-                        foregroundColor: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16),
+                  SizedBox(height: 118),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Welcome to Tasky",
+                        style: TextStyle(
+                          color: Color(0xFFFFFCFC),
+                          fontSize: 24,
+                          fontWeight: FontWeight.w400,
                         ),
                       ),
-                      child: Text(
-                        "Let's Get Started",
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight(500),
+                      SvgPicture.asset("assets/images/waving-hand.svg"),
+                    ],
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    "Your productivity journey starts here.",
+                    style: TextStyle(
+                      color: Color(0xFFFFFCFC),
+                      fontWeight: FontWeight.w400,
+                      fontSize: 16,
+                    ),
+                  ),
+                  SizedBox(height: 24),
+                  SvgPicture.asset(
+                    "assets/images/work-in-progress.svg",
+                    width: 215,
+                    height: 200,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 28),
+                        Text(
+                          "Full Name",
+                          style: TextStyle(
+                            color: Color(0xFFFFFCFC),
+                            fontWeight: FontWeight.w400,
+                            fontSize: 16,
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        TextFormField(
+                          style: TextStyle(color: Colors.white),
+                          controller: textEditingController,
+                          validator: (String? value) {
+                            if(value == null || value.trim().isEmpty){
+                              return "Please Enter your Full Name";
+                            }
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                            hintText: "e.g. Ahmed Alaa",
+                            hintStyle: TextStyle(color: Color(0xFF6D6D6D)),
+                            filled: true,
+                            fillColor: Color(0xFF282828),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(16),
+                              borderSide: BorderSide.none,
+                            ),
+                          ),
+                          cursorColor: Colors.white,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 28),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: SizedBox(
+                      height: 40,
+                      width: MediaQuery
+                          .of(context)
+                          .size
+                          .width,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (_key.currentState?.validate() ?? false) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (BuildContext context) {
+                                    return HomeScreen();
+                                  }),
+                            );
+                          } else {
+                            //snack bar
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xFF15B86C),
+                          foregroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                        ),
+                        child: Text(
+                          "Let's Get Started",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight(500),
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
